1d60019366797a14a55d1ac44b346dc147f1690b
John W <91273156+BoltR6@users.noreply.github.com>
2023-03-18 19:27:10 +0000

    Update flake.nix

--- pros/flake.nix
+++ pros/flake.nix
@@ -1,1 +1,21 @@
-s
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+  outputs = { self, nixpkgs }: 
+    let
+      pkgs = import nixpkgs { 
+        system = "x86_64-linux";
+      };
+    in
+    {
+    devShells.x86_64-linux.default = with pkgs; mkShell {
+      buildInputs = [ python310 gcc-arm-embedded ];
+      shellHook = 
+        ''
+        source enviro/bin/activate
+        '';
+      
+    };
+  };
+}
